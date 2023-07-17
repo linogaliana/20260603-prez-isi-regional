42ce486a131e8d6fe45c4dd87805411fe52c3af4
Romain Puyau <romain.puyau@insee.fr>
2023-07-17 14:30:34 +0200

    ajout fille dev avec liste dependances + modif read.me apres test render sur AUS + set config AUS

---
+++ dev.R
@@ -0,0 +1,10 @@
+# Liste des dépendances à installer
+
+install.packages("jsonlite")
+install.packages("htmltools")
+install.packages("digest")
+install.packages("knitr")
+install.packages("rmarkdown")
+install.packages("dplyr")
+install.packages("DT")
+install.packages("plotly")
